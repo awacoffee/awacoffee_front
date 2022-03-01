7444464fe7cb594968947c669b7af0a894082345
yoshitomi9000 <shockn9000@gmail.com>
2022-03-01 20:26:31 +0900

    column_htmlの不具合になっていそうなHunkを削除
    よしとみ

--- ishikawa_keep/assets/js/column_list.js
+++ ishikawa_keep/assets/js/column_list.js
@@ -1,23 +1,23 @@
 // ▼店舗情報セクション／タブ切り替え▼
 function GethashID(hashIDName) {
-    if (hashIDName) {
-        $(".column_tab_wrap li h2")
-            .find("a")
-            .each(function () {
-                let idName = $(this).attr("href");
-                if (idName == hashIDName) {
-                    let parentElm = $(this).parent();
-                    $(".column_tab_wrap li h2").removeClass("column_active");
-                    $(parentElm).addClass("column_active");
-                    $(".cards_wrap").removeClass("is_column_active");
-                    $(hashIDName).addClass("is_column_active");
-                }
-            });
-    }
+  if (hashIDName) {
+    $(".column_tab_wrap li")
+      .find("a")
+      .each(function () {
+        let idName = $(this).attr("href");
+        if (idName == hashIDName) {
+          let parentElm = $(this).parent();
+          $(".column_tab_wrap li").removeClass("column_active");
+          $(parentElm).addClass("column_active");
+          $(".cards_wrap").removeClass("is_column_active");
+          $(hashIDName).addClass("is_column_active");
+        }
+      });
+  }
 }
 
 $(".column_item").on("click", function () {
-    let idName = $(this).attr("href");
-    GethashID(idName);
-    return false;
+  let idName = $(this).attr("href");
+  GethashID(idName);
+  return false;
 });
