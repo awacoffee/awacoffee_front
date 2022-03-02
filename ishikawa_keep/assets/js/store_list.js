979c06e1b546ceaabae8bac3555e313c3fbdfd78
yoshitomi9000 <99855575+yoshitomi9000@users.noreply.github.com>
2022-03-02 17:52:34 +0900

    store_list.jsの訂正
    よしとみ

--- ishikawa_keep/assets/js/store_list.js
+++ ishikawa_keep/assets/js/store_list.js
@@ -46,75 +46,8 @@
 function pcMoreBtn() {
     const show = 3; //初期表示枚数
     const moreNum = 3; //追加カード枚数
-    let cityLength = $(".city_area .pc_stores_cards .stores_card").length
-    let eastLength = $(".east_area .pc_stores_cards .stores_card").length
-    let westLength = $(".west_area .pc_stores_cards .stores_card").length
-    let southLength = $(".south_area .pc_stores_cards .stores_card").length
-
-    // 徳島市の店舗カードmoreボタン
-    if (cityLength <= show) {
-        $(".city_area .more_btn").fadeOut();
-    } else {
-$(".city_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
-    $(".city_area .more_btn").on("click", function () {
-        $(this)
-            .prev(".store_lists")
-            .find(".stores_card.is-hidden")
-            .slice(0, moreNum)
-            .removeClass("is-hidden");
-        if (
-            $(this).prev(".store_lists").find(".stores_card.is-hidden")
-                .length == 0
-        ) {
-            $(this).fadeOut();
-        }
-    });
-    };
-
-        // 東部の店舗カードmoreボタン
-    if (eastLength <= show) {
-        $(".east_area .more_btn").fadeOut();
-    } else {
-$(".east_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
-    $(".east_area .more_btn").on("click", function () {
-        $(this)
-            .prev(".store_lists")
-            .find(".stores_card.is-hidden")
-            .slice(0, moreNum)
-            .removeClass("is-hidden");
-        if (
-            $(this).prev(".store_lists").find(".stores_card.is-hidden")
-                .length == 0
-        ) {
-            $(this).fadeOut();
-        }
-    });
-    };
-        // 西部の店舗カードmoreボタン
-    if (westLength <= show) {
-        $(".west_area .more_btn").fadeOut();
-    } else {
-$(".west_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
-    $(".west_area .more_btn").on("click", function () {
-        $(this)
-            .prev(".store_lists")
-            .find(".stores_card.is-hidden")
-            .slice(0, moreNum)
-            .removeClass("is-hidden");
-        if (
-            $(this).prev(".store_lists").find(".stores_card.is-hidden")
-                .length == 0
-        ) {
-            $(this).fadeOut();
-        }
-    });
-    };
-        // 南部の店舗カードmoreボタン
-    if (southLength <= show) {
-        $(".south_area .more_btn").fadeOut();
-    } else {
-$(".south_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
-    $(".south_area .more_btn").on("click", function () {
+    $(".stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden"); //初期表示枚数以降のカードにis-hiddenをつける
+    $(".more_btn").on("click", function () {
         $(this)
             .prev(".store_lists")
             .find(".stores_card.is-hidden")
