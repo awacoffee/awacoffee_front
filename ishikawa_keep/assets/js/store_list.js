ce7dc41457888a944286de85d41c96919ed4fa5f
ishikawa-saori <mawe.na.mito.design@gmail.com>
2022-03-03 20:43:10 +0900

    吉富さんより ・ニュース一覧（html、css、js。jsが最新ではない？） ・店舗一覧（htmlとjs）

--- ishikawa_keep/assets/js/store_list.js
+++ ishikawa_keep/assets/js/store_list.js
@@ -1,53 +1,21 @@
 "use strict";
 
-// ▼moreボタンでさらに表示▼
-
-// 例えば徳島市ブロックの中にあるmoreボタンは徳島市ブロックだけに反応してほしい
-// 押されたボタンの兄弟要素を取得してfind →出ない。
-
-// var show = 3; //初期表示件数
-// var num = 3; //追加していく件数（仮）
-// var contents = ".store_lists article";
-// $(contents + ":nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
-// $(".more_btn").on("click", function () {
-//     $(this)
-//         .siblings(".store_lists") // btnの兄弟要素全て取得
-//         .find(contents + ".is-hidden")
-//         .slice(0, num)
-//         .removeClass("is-hidden"); // num個切り出して表示
-//     if (
-//         $(this)
-//             .siblings(".store_lists")
-//             .find(contents + ".is-hidden").length == 0
-//     ) {
-//         $(this).fadeOut(); // is-hiddenついているものがなくなったらbtn消える
-//     }
-// });
-
-// ▼moreボタンでさらに表示▼
-// 出るには出てる（出てくる枚数がおかしい気はする）
-// moreボタン押した時の判定がエリアの垣根を超えてしまう
-
-// var show = 3; //初期表示件数
-// var num = 3; //追加していく件数（仮）
-// var contents = ".store_lists article";
-// $(contents + ":nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
-// $(".more_btn").on("click", function () {
-//     $(contents + ".is-hidden")
-//         .slice(0, num)
-//         .removeClass("is-hidden");
-//     if ($(contents + ".is-hidden").length == 0) {
-//         $(".more_btn").fadeOut(); // is-hiddenついているものがなくなったらbtn消える
-//     }
-// });
-
 // =========pcのカードのmoreボタン================================================
 
 function pcMoreBtn() {
     const show = 3; //初期表示枚数
     const moreNum = 3; //追加カード枚数
-    $(".stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden"); //初期表示枚数以降のカードにis-hiddenをつける
-    $(".more_btn").on("click", function () {
+    let cityLength = $(".city_area .pc_stores_cards .stores_card").length
+    let eastLength = $(".east_area .pc_stores_cards .stores_card").length
+    let westLength = $(".west_area .pc_stores_cards .stores_card").length
+    let southLength = $(".south_area .pc_stores_cards .stores_card").length
+
+    // 徳島市の店舗カードmoreボタン
+    if (cityLength <= show) {
+        $(".city_area .more_btn").fadeOut();
+    } else {
+$(".city_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+    $(".city_area .more_btn").on("click", function () {
         $(this)
             .prev(".store_lists")
             .find(".stores_card.is-hidden")
@@ -60,6 +28,65 @@ function pcMoreBtn() {
             $(this).fadeOut();
         }
     });
+    };
+
+        // 東部の店舗カードmoreボタン
+    if (eastLength <= show) {
+        $(".east_area .more_btn").fadeOut();
+    } else {
+$(".east_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+    $(".east_area .more_btn").on("click", function () {
+        $(this)
+            .prev(".store_lists")
+            .find(".stores_card.is-hidden")
+            .slice(0, moreNum)
+            .removeClass("is-hidden");
+        if (
+            $(this).prev(".store_lists").find(".stores_card.is-hidden")
+                .length == 0
+        ) {
+            $(this).fadeOut();
+        }
+    });
+    };
+        // 西部の店舗カードmoreボタン
+    if (westLength <= show) {
+        $(".west_area .more_btn").fadeOut();
+    } else {
+$(".west_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+    $(".west_area .more_btn").on("click", function () {
+        $(this)
+            .prev(".store_lists")
+            .find(".stores_card.is-hidden")
+            .slice(0, moreNum)
+            .removeClass("is-hidden");
+        if (
+            $(this).prev(".store_lists").find(".stores_card.is-hidden")
+                .length == 0
+        ) {
+            $(this).fadeOut();
+        }
+    });
+    };
+        // 南部の店舗カードmoreボタン
+    if (southLength <= show) {
+        $(".south_area .more_btn").fadeOut();
+    } else {
+$(".south_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+    $(".south_area .more_btn").on("click", function () {
+        $(this)
+            .prev(".store_lists")
+            .find(".stores_card.is-hidden")
+            .slice(0, moreNum)
+            .removeClass("is-hidden");
+        if (
+            $(this).prev(".store_lists").find(".stores_card.is-hidden")
+                .length == 0
+        ) {
+            $(this).fadeOut();
+        }
+    });
+    };
 }
 
 // =========spのカードのmoreボタン================================================
@@ -67,9 +94,17 @@ function pcMoreBtn() {
 function spMoreBtn() {
     const show = 4; //初期表示枚数
     const moreNum = 4; //追加カード枚数
+    let cityLength = $(".city_area .sp_stores_cards .stores_card").length
+    let eastLength = $(".east_area .sp_stores_cards .stores_card").length
+    let westLength = $(".west_area .sp_stores_cards .stores_card").length
+    let southLength = $(".south_area .sp_stores_cards .stores_card").length
 
-    $(".stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
-    $(".more_btn").on("click", function () {
+   // 徳島市の店舗カードmoreボタン
+    if (cityLength <= show) {
+        $(".city_area .more_btn").fadeOut();
+    } else {
+$(".city_area .sp_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+    $(".city_area .more_btn").on("click", function () {
         $(this)
             .prev(".store_lists")
             .find(".stores_card.is-hidden")
@@ -82,14 +117,216 @@ function spMoreBtn() {
             $(this).fadeOut();
         }
     });
+    };
+
+        // 東部の店舗カードmoreボタン
+    if (eastLength <= show) {
+        $(".east_area .more_btn").fadeOut();
+    } else {
+$(".east_area .sp_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+    $(".east_area .more_btn").on("click", function () {
+        $(this)
+            .prev(".store_lists")
+            .find(".stores_card.is-hidden")
+            .slice(0, moreNum)
+            .removeClass("is-hidden");
+        if (
+            $(this).prev(".store_lists").find(".stores_card.is-hidden")
+                .length == 0
+        ) {
+            $(this).fadeOut();
+        }
+    });
+    };
+        // 西部の店舗カードmoreボタン
+    if (westLength <= show) {
+        $(".west_area .more_btn").fadeOut();
+    } else {
+$(".west_area .sp_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+    $(".west_area .more_btn").on("click", function () {
+        $(this)
+            .prev(".store_lists")
+            .find(".stores_card.is-hidden")
+            .slice(0, moreNum)
+            .removeClass("is-hidden");
+        if (
+            $(this).prev(".store_lists").find(".stores_card.is-hidden")
+                .length == 0
+        ) {
+            $(this).fadeOut();
+        }
+    });
+    };
+
+        // 南部の店舗カードmoreボタン
+    if (southLength <= show) {
+        $(".south_area .more_btn").fadeOut();
+    } else {
+$(".south_area .sp_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+    $(".south_area .more_btn").on("click", function () {
+        $(this)
+            .prev(".store_lists")
+            .find(".stores_card.is-hidden")
+            .slice(0, moreNum)
+            .removeClass("is-hidden");
+        if (
+            $(this).prev(".store_lists").find(".stores_card.is-hidden")
+                .length == 0
+        ) {
+            $(this).fadeOut();
+        }
+    });
+    };
+
 }
 
 // ロード時のwindow幅に応じてmoreボタンの関数を分ける　これをリアルタイムに切り替わるようにしたい
 
-let windowSize = $(window).width();
+// let windowSize = $(window).width();
 
-if (windowSize < 768) {
-    spMoreBtn();
-} else {
+// if (windowSize < 768) {
+//     spMoreBtn();
+// } else {
+//     pcMoreBtn();
+// }
+
+
+$(window).on("load resize", function () {
+ let window_w = window.innerWidth;
+ if (window_w > 768) {
     pcMoreBtn();
-}
+ } else {
+spMoreBtn();
+ }
+});
+
+
+
+
+// // =========pcのカードのmoreボタン================================================
+
+// function pcMoreBtn() {
+//     const show = 3; //初期表示枚数
+//     const moreNum = 3; //追加カード枚数
+
+//     function hiddenCut() {
+//         $(this)
+//             .prev(".store_lists")
+//             .find(".stores_card.is-hidden")
+//             .slice(0, moreNum)
+//             .removeClass("is-hidden");
+//         if (
+//             $(this).prev(".store_lists").find(".stores_card.is-hidden")
+//                 .length == 0
+//         ) {
+//             $(this).fadeOut();
+//         }
+//     };
+
+//     // 徳島市の店舗カードmoreボタン
+//     if (cityLength <= show) {
+//         $(".city_area .more_btn").fadeOut();
+//     } else {
+// $(".city_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+//         $(".city_area .more_btn").on("click",hiddenCut);
+//     };
+
+//     // 東部の店舗カードmoreボタン
+//     if (eastLength <= show) {
+//         $(".east_area .more_btn").fadeOut();
+//     } else {
+// $(".east_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+//         $(".east_area .more_btn").on("click",hiddenCut);
+//     };
+
+//     // 西部の店舗カードmoreボタン
+//     if (westLength <= show) {
+//         $(".west_area .more_btn").fadeOut();
+//     } else {
+// $(".west_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+//     $(".west_area .more_btn").on("click",hiddenCut);
+//     };
+
+//     // 南部の店舗カードmoreボタン
+//     if (southLength <= show) {
+//         $(".south_area .more_btn").fadeOut();
+//     } else {
+// $(".south_area .pc_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+//         $(".south_area .more_btn").on("click",hiddenCut);
+//     };
+// }
+
+// // =========spのカードのmoreボタン================================================
+
+// function spMoreBtn() {
+//     const show = 4; //初期表示枚数
+//     const moreNum = 4; //追加カード枚数
+
+//     function hiddenCut() {
+//         $(this)
+//             .prev(".store_lists")
+//             .find(".stores_card.is-hidden")
+//             .slice(0, moreNum)
+//             .removeClass("is-hidden");
+//         if (
+//             $(this).prev(".store_lists").find(".stores_card.is-hidden")
+//                 .length == 0
+//         ) {
+//             $(this).fadeOut();
+//         }
+//     }
+
+//    // 徳島市の店舗カードmoreボタン
+//     if (cityLength <= show) {
+//         $(".city_area .more_btn").fadeOut();
+//     } else {
+// $(".city_area .sp_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+//     $(".city_area .more_btn").on("click",hiddenCut);
+//     };
+
+//     // 東部の店舗カードmoreボタン
+//     if (eastLength <= show) {
+//         $(".east_area .more_btn").fadeOut();
+//     } else {
+// $(".east_area .sp_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+//     $(".east_area .more_btn").on("click",hiddenCut);
+//     };
+
+//     // 西部の店舗カードmoreボタン
+//     if (westLength <= show) {
+//         $(".west_area .more_btn").fadeOut();
+//     } else {
+// $(".west_area .sp_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+//     $(".west_area .more_btn").on("click",hiddenCut);
+//     };
+
+//     // 南部の店舗カードmoreボタン
+//     if (southLength <= show) {
+//         $(".south_area .more_btn").fadeOut();
+//     } else {
+// $(".south_area .sp_stores_cards .stores_card:nth-child(n + " + (show + 1) + ")").addClass("is-hidden");
+//     $(".south_area .more_btn").on("click",hiddenCut);
+//     };
+// }
+
+// // =========変数の設定================================================
+// let area1 = $(".city_area");
+// let area2 = $(".east_area");
+// let area3 = $(".west_area");
+// let area4 = $(".south_area");
+
+// let cityLength = $(".city_area .pc_stores_cards .stores_card").length;
+// let eastLength = $(".east_area .pc_stores_cards .stores_card").length;
+// let westLength = $(".west_area .pc_stores_cards .stores_card").length;
+// let southLength = $(".south_area .pc_stores_cards .stores_card").length;
+
+// // ========ロード時のwindow幅に応じてmoreボタンの関数を分ける========
+
+// $(window).on("load resize", function () {
+//  let window_w = window.innerWidth;
+//  if (window_w > 768) {
+//     pcMoreBtn();
+//  } else {
+// spMoreBtn();
+//  }
+// });
