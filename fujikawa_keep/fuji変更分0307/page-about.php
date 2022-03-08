547659bb2682977745db35561821b7430f8ac321
fujikawa <>
2022-03-08 14:26:11 +0900

    a

---
+++ fujikawa_keep/fuji変更分0307/page-about.php
@@ -0,0 +1,115 @@
+<?php get_header(); ?>
+<main id="about_under">
+  <div class="header_img_bg mainvisual_wrap"></div>
+  <!-- ▼パンクズ挿入▼ -->
+  <!-- ▼タイトルエリア▼ -->
+  <div class="wrap">
+    <div class="under_title inner">
+      <div class="under_wrap_title">
+        <p>about</p>
+        <h1>このサイトについて</h1>
+      </div>
+    </div>
+  </div>
+  <!-- 一つ目削除？ -->
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="under_about_sec_title">運営者について</h2>
+        </div>
+        <div class="wrap_text">
+          <p class="under_about_text">
+            このサイトは国の求職者支援制度に基づく、株式会社QLIPプログラミングスクール・Webプログラマー養成科第12期生、チーム「動（どう）」が、2022年に制作したウェブサイト（以下「本サイト」と記載）です。
+            全国的に見て、徳島県民のコーヒー消費量が多いという背景からか、県内各所にはそのニーズに応えるためのこだわりのカフェや喫茶店が多く存在しています。
+            当サイトではコーヒーをキーワードに据えて徳島県の魅力発信をすべく、県内のこだわりのカフェや喫茶店の魅力を紹介しています。
+            また近年、コロナ禍において自宅で過ごす時間が増加していることから、コーヒーを自宅で楽しむために必要な情報も合わせて発信しています。
+            これによりコロナ禍における在宅ワークや自宅で過ごす時間の豊かさをサポートすることも目的としています。
+            掲載店舗の情報・コラム内容についてはできるだけ正確を期していますが、社会状況などにより変更が生じる場合があるため、当サイトが責任を負うものではありません。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="under_about_sec_title">このサイトについて</h2>
+        </div>
+        <div class="wrap_text">
+          <p class="under_about_text">
+            このサイトは国の求職者支援制度に基づく、株式会社QLIPプログラミングスクール・Webプログラマー養成科第12期生、チーム「動（どう）」が、2022年に制作したウェブサイト（以下「本サイト」と記載）です。
+            全国的に見て、徳島県民のコーヒー消費量が多いという背景からか、県内各所にはそのニーズに応えるためのこだわりのカフェや喫茶店が多く存在しています。
+            当サイトではコーヒーをキーワードに据えて徳島県の魅力発信をすべく、県内のこだわりのカフェや喫茶店の魅力を紹介しています。
+            また近年、コロナ禍において自宅で過ごす時間が増加していることから、コーヒーを自宅で楽しむために必要な情報も合わせて発信しています。
+            これによりコロナ禍における在宅ワークや自宅で過ごす時間の豊かさをサポートすることも目的としています。
+            掲載店舗の情報・コラム内容についてはできるだけ正確を期していますが、社会状況などにより変更が生じる場合があるため、当サイトが責任を負うものではありません。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="under_about_sec_title">
+            本サイトコンテンツの無許可の転載・複製・転用等について
+          </h2>
+        </div>
+        <div class="wrap_text">
+          <p class="under_about_text">
+            本サイトの内容、テキスト、画像等の無断転載・無断使用を固く禁じます。
+            また、まとめサイト等への引用を厳禁いたします。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="under_about_sec_title">免責</h2>
+        </div>
+        <div class="wrap_text">
+          <p class="under_about_text">
+            当グループは、本サービスについて、その正確性・継続性・機能性・有用性・完全性等についていかなる保証もいたしません。当グループは、本サービスの利用に関して利用者に生じた損害については何らの責任を負いません。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="under_about_sec_title">
+            サービスの中止・中断・変更・終了
+          </h2>
+        </div>
+        <div class="wrap_text">
+          <p class="under_about_text">
+            当グループは、利用者の事前の同意を得ることなく、予告なしにいつでも本サービスの全部もしくは一部を変更し、または本サービスの提供を中断もしくは終了できるものとします。当グループは、本サービスの全部もしくは一部の変更、提供の中断もしくは終了によって生じた、利用者およびその他第三者に生じた損害につき、一切その責任を負わないものとします。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="under_about_sec_title">利用規約の改定</h2>
+        </div>
+        <div class="wrap_text">
+          <p class="under_about_text">
+            当グループは、事前に通知することなく、本規約の内容を変更することがあります。変更した内容は、本サイト上に掲載することとし、当該掲載の時から即時に効力を生じ、本サービスの利用について適用されることとします。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer(); ?>
---
+++ fujikawa_keep/fuji変更分0307/page-privacy.php
@@ -0,0 +1,149 @@
+<?php get_header(); ?>
+<main id="privacy_under">
+  <div class="header_img_bg mainvisual_wrap"></div>
+  <!-- ▼パンクズ挿入▼ -->
+  <!-- ▼タイトルエリア▼ -->
+  <div class="wrap">
+    <div class="under_title inner">
+      <div class="under_wrap_title">
+        <p>Privacy policy</p>
+        <h1>プライバシーポリシー</h1>
+      </div>
+    </div>
+    <!-- wrapをひとまとめにするか -->
+    <!-- </div> -->
+    <!-- <div class="wrap"> -->
+    <div class="inner">
+      <div class="wrap_text">
+        <p class="privacy_text">
+          本プライバシーポリシーは、『Awa Our Coffee
+          Hour~とくしまとコーヒー、キミと見つける美
+          味しいひととき~』(以下「本サイト」といいます。)
+          の各種サービス(本サイトによる情報
+          提供、お問い合わせ等)において、本サイトを利用する方々の個人情報およびそれに準ずる情
+          報を取り扱う際に、本サイトが遵守する方針を示したものです。
+        </p>
+      </div>
+    </div>
+  </div>
+  <!-- section-->
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="privacy_sec_title">第1条<span>基本方針</span></h2>
+        </div>
+        <div class="wrap_text">
+          <p class="privacy_text">
+            本サイトでは、個人情報の重要性を認識し、個人情報を保護することが社会的責務であると考
+            え、個人情報に関する法令を遵守し、本サイトで取り扱う個人情報の取得、利用、管理を適正
+            に行います。
+            本サイトで収集した情報は、利用目的の範囲内で適切に取り扱います。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="privacy_sec_title">
+            第2条<span>収集する情報の範囲</span>
+          </h2>
+        </div>
+        <div class="wrap_text">
+          <p class="privacy_text">
+            (1)本サイトでは、インターネットドメイン名、IPアドレス、当サイトの閲覧等の情報(以下
+            「利用者の情報」という。)を自動的に収集しています。なお、Cookie(サーバ側で利用者を
+            識別するために、サーバから利用者のブラウザに送信され、利用者のコンピュータに蓄積され
+            る情報)は、ユーザビリティーの向上を目的とする内容に限定するものであり、個人情報は一
+            切含みません。また、利用者のブラウザの設定によりCookieの機能を無効にすることができま
+            す。
+            (2)本サイトでは、お問い合わせページの入力フォームよりご連絡をいただいた場合
+            に、氏名、メールアドレスおよびお問い合わせ内容を収集します。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="privacy_sec_title">第3条<span>利用目的</span></h2>
+        </div>
+        <div class="wrap_text">
+          <p class="privacy_text">
+            第2条の(1)において収集した情報は、本サイトが提供するサービスを円滑に運営するために
+            利用します。
+            第2条の(2)において収集した情報は、お問い合わせの回答や確認の連絡のために利用しま
+            す。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="privacy_sec_title">第4条<span>情報管理</span></h2>
+        </div>
+        <div class="wrap_text">
+          <p class="privacy_text">
+            利用者からご提供いただいた情報については、常に正確かつ最新の情報となるよう努めます。
+            本サイトは、個人情報の漏えいや滅失又は棄損を防止するために、適切なセキリュティ対策を
+            実施して個人情報を保護します。また、個人情報が不要となった場合には、すみやかに廃棄し
+            ます。
+            本サイトは、利用者からご提供いただいた個人情報を、不正アクセス、脅迫等の違法行
+            為があった場合及びその他特別の理由のある場合を除き、利用者本人の同意を得ることなく
+            「第3条
+            利用目的」以外の目的のために自ら利用および、第三者に提供することはいたしませ
+            ん。
+            お問い合わせにより本サイトに送られた情報とそのメタデータは、サイト運営期間中は保
+            存いたします。本サイトの管理人は、利用者から利用者本人の個人情報を「消去してほしい」
+            というリクエストに応えることが出来ます。これには、管理や法律、セキュリティ目的のため
+            に保持する義務がある情報は含まれません。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="privacy_sec_title">第5条<span>適用範囲</span></h2>
+        </div>
+        <div class="wrap_text">
+          <p class="privacy_text">
+            本プライバシーポリシーは、本サイトにおいてのみ適用されます。
+            本サイトの外部リンク(他
+            サイトへのリンク)先のサイトにおける個人情報の取扱いについては、リンク先のサイトが規
+            定を定めています。リンク先のプライバシーポリシーをご確認下さい。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wrap">
+      <div class="inner">
+        <div class="wrap_title">
+          <h2 class="privacy_sec_title">
+            第6条<span>内容の変更について</span>
+          </h2>
+        </div>
+        <div class="wrap_text">
+          <p class="privacy_text">
+            本サイトは、個人情報に関して適用される日本の法令を遵守するとともに、本プライバシーポ
+            リシーの内容を適宜見直し改善に努めていくため、予告なく修正する場合がございます。修正
+            した最新のプライバシーポリシーは常に本ページにて開示いたします。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer(); ?>
